75c930f01856e1c0c09dae2dffbd22b50192b38b
amirali <alimoradi7718@gmail.com>
2025-10-26 12:07:24 +0330

    fix: correct conditional rendering for products table
    - Show WarehousePartsTable only when status is "success" and data has items
    - Show NoProducts component when no items or fetch fails

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,11 +1,19 @@
 import WarehousePartsTable from "@/components/WarehousePartsTable/WarehousePartsTable";
 import { fetchProducts } from "./services/fetchProducts";
+import NoProducts from "./components/NoProducts";
 
 async function Productspage() {
-  const products = await fetchProducts();
+  const { data, message, status } = await fetchProducts();
+
+  console.log(data);
+
   return (
     <div className="w-full space-y-2 overflow-x-auto">
-      <WarehousePartsTable products={products.data} />
+      {status === "success" && data.length > 0 ? (
+        <WarehousePartsTable products={data} />
+      ) : (
+        <NoProducts message={message} />
+      )}
     </div>
   );
 }
