ec4d9fa16e068ce3394b2657e96a0f6a30ce4308
AMIRALI <alimoradi7718@gmail.com>
2025-11-27 20:06:45 +0330

    fix: make supplier action buttons full width on mobile and fixed width on desktop

--- src/app/suppliers/components/SuppliersActions/SuppliersActions.jsx
+++ src/app/suppliers/components/SuppliersActions/SuppliersActions.jsx
@@ -3,27 +3,27 @@ import { Button } from "@/components/ui/button";
 
 function SuppliersActions() {
   return (
-    <div className="flex flex-wrap gap-3 mt-4 justify-start">
-      <Link href="/suppliers/new">
-        <Button className="bg-primary text-secondary-foreground px-4 sm:px-6 py-2 sm:py-3 rounded-lg w-full sm:w-auto text-center flex-1 sm:flex-auto min-w-[150px]">
+    <div className="flex flex-wrap gap-4 mt-4 justify-start">
+      <Link href="/suppliers/new" className="w-full sm:w-[200px]">
+        <Button className="w-full px-4 sm:px-6 py-2 sm:py-3 rounded-lg bg-primary text-secondary-foreground text-center">
           افزودن تأمین‌کننده
         </Button>
       </Link>
 
-      <Link href="/suppliers/delete">
-        <Button className="bg-red-500 text-secondary-foreground   px-4 sm:px-6 py-2 sm:py-3 rounded-lg w-full sm:w-auto text-center flex-1 sm:flex-auto min-w-[150px]">
+      <Link href="/suppliers/delete" className="w-full sm:w-[200px]">
+        <Button className="w-full px-4 sm:px-6 py-2 sm:py-3 rounded-lg bg-red-500 text-secondary-foreground text-center">
           حذف تأمین‌کننده
         </Button>
       </Link>
 
-      <Link href="/suppliers/edit">
-        <Button className="bg-yellow-500 text-secondary-foreground px-4 sm:px-6 py-2 sm:py-3 rounded-lg w-full sm:w-auto text-center flex-1 sm:flex-auto min-w-[150px]">
+      <Link href="/suppliers/edit" className="w-full sm:w-[200px]">
+        <Button className="w-full px-4 sm:px-6 py-2 sm:py-3 rounded-lg bg-yellow-500 text-secondary-foreground text-center">
           ویرایش تأمین‌کننده
         </Button>
       </Link>
 
-      <Link href="/suppliers/details">
-        <Button className="bg-blue-500 text-secondary-foreground px-4 sm:px-6 py-2 sm:py-3 rounded-lg w-full sm:w-auto text-center flex-1 sm:flex-auto min-w-[150px]">
+      <Link href="/suppliers/details" className="w-full sm:w-[200px]">
+        <Button className="w-full px-4 sm:px-6 py-2 sm:py-3 rounded-lg bg-blue-500 text-secondary-foreground text-center">
           اطلاعات تأمین‌کنندگان
         </Button>
       </Link>
